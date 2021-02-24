951ad8869dc5833036d1e0cedaeca5c1bb46963a
pabbs18 <prabhanjanadesai@gmail.com>
2021-02-24 17:17:15 +0530

    Initial commit

---
+++ src/test/java/com/project/retroboard/RetroboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.retroboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
